77a1745f8e71948c9345919a6392d4f62a5b0592
kcuevas <cuev73@live.com>
2023-10-17 21:52:05 -0400

    fixed App import statement in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
